7ca61736a05c353edac90dffe7869374d9851d8c
Jonathan Hunt <git@huntdesign.co.nz>
2016-08-24 17:13:33 +1200

    Implement hook_preprocess_block to add bootstrap class to latest news block.

--- docroot/sites/all/themes/riversorgnz/template.php
+++ docroot/sites/all/themes/riversorgnz/template.php
@@ -3,3 +3,19 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Pre-processes variables for the "block" theme hook.
+ *
+ * See template for list of available variables.
+ *
+ * @see block.tpl.php
+ *
+ * @ingroup theme_preprocess
+ */
+function riversorgnz_preprocess_block(&$variables) {
+  // Override Latest news view block to add Bootstrap classes.
+  if ($variables['block_html_id'] == 'block-views-rivers-news-block-1') {
+    $variables['classes_array'][] = 'col-md-6';
+  }
+}
