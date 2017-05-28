9550e6afd9520717225cc6a23143d9295f6afb79
Jonathan Hunt <git@huntdesign.co.nz>
2017-05-28 16:13:40 +1200

    #43 Latest news and calendar blocks both col-md-6.

--- docroot/sites/all/themes/riversorgnz/template.php
+++ docroot/sites/all/themes/riversorgnz/template.php
@@ -15,7 +15,10 @@
  */
 function riversorgnz_preprocess_block(&$variables) {
   // Override Latest news view block to add Bootstrap classes.
-  if ($variables['block_html_id'] == 'block-views-rivers-news-latest-news') {
-    $variables['classes_array'][] = 'col-md-6';
+  switch ($variables['block_html_id']) {
+    case 'block-views-rivers-calendar-block-2':
+    case 'block-views-rivers-news-latest-news':
+      $variables['classes_array'][] = 'col-md-6';
+      break;
   }
 }
