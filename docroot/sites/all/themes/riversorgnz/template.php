84971377c4be29ded561af89e517457c80114748
Jonathan Hunt <git@huntdesign.co.nz>
2017-06-04 20:38:49 +1200

    #13 Show River name in Section page title.

--- docroot/sites/all/themes/riversorgnz/template.php
+++ docroot/sites/all/themes/riversorgnz/template.php
@@ -5,6 +5,30 @@
  */
 
 /**
+ * Implements template_preprocess_page().
+ * @param $variables
+ */
+function riversorgnz_preprocess_page(&$variables) {
+
+  // Add River name (and Alternate name) to page h1 title.
+  if (isset($variables['node'])) {
+    $node = $variables['node'];
+    if ($node->type == 'section') {
+
+      $river_name = $node->field_river_name[LANGUAGE_NONE][0]['value'];
+      //$variables['title_prefix'] = l($river_name, 'nz/' . $alias) . ' / ';
+
+      $variables['title'] = check_plain($river_name) . ' / ' . check_plain($node->title);
+
+      if (!empty($node->field_alternate_name[LANGUAGE_NONE][0]['value'])) {
+        $variables['title'] .= ' (' . check_plain($node->field_alternate_name[LANGUAGE_NONE][0]['value']) . ')';
+      }
+    }
+  }
+}
+
+
+/**
  * Pre-processes variables for the "block" theme hook.
  *
  * See template for list of available variables.
