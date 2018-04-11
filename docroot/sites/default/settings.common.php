10902f84a44b622759bac53a1d49e81566233829
Jonathan Hunt <git@huntdesign.co.nz>
2018-04-11 15:53:26 +1200

    #12 Define new Staging config for Catalyst Cloud.

--- docroot/sites/default/settings.common.php
+++ docroot/sites/default/settings.common.php
@@ -13,8 +13,13 @@ switch ($_SERVER['HTTP_HOST']) {
     break;
 
   case 'd7.rivers.org.nz':
+    $conf['environment'] = 'uat';
+    break;
+
+  case 'rivers.catalystdemo.net.nz':
     $conf['environment'] = 'staging';
     break;
+
 
   default:
     $conf['environment'] = 'production';
@@ -59,7 +64,63 @@ switch ($conf['environment']) {
     $conf['environment_indicator_overwritten_fixed'] = FALSE;
     break;
 
+  // Catalyst CHCSC staging.
   case 'staging':
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('memory_limit', '156M');
+    // c/- PreviousNext
+    // Memory allocation to be 256MB. This is to cover cron etc.
+    if (isset($_GET['q']) && (strpos($_GET['q'], 'admin') === 0 || strpos($_GET['q'], 'en/admin') === 0)) {
+      ini_set('memory_limit', '196M');
+    }
+    // Node edit pages are memory heavy too.
+    if (isset($_GET['q']) && preg_match('@^node\/([0-9]+)\/edit$@', $_GET['q'])) {
+      ini_set('memory_limit', '196M');
+    }
+
+    // Memory allocation to be 256MB. This is to cover cron etc.
+    if (isset($_GET['q']) && (strpos($_GET['q'], 'batch') === 0)) {
+      ini_set('memory_limit', '196M');
+    }
+
+    // File system.
+    //$conf['file_private_path'] = '/home/rivers/files_rivers_dev';
+    $conf['file_directory_path'] = 'sites/default/files';
+
+    $conf['reverse_proxy'] = true;
+    $conf['reverse_proxy_header'] = 'HTTP_X_FORWARDED_FOR';
+
+    // Each element of this array is the IP address of any of your reverse proxies.
+    $conf['reverse_proxy_addresses'] = array('127.0.0.1');
+
+    // Connection details for Catalyst Squid proxies.
+    $conf['proxy_server'] = 'ext-proxy-staging.catalyst.net.nz';
+    $conf['proxy_port'] = '3128';
+    $conf['proxy_exceptions'] = array('cat-chcsc-staging-solr.servers.catalyst.net.nz');
+
+    $conf['omit_vary_cookie'] = true;
+
+
+    // Flog.
+    $conf['flog_path'] = $conf['file_directory_path'];
+    $conf['flog_enabled'] = TRUE;
+
+    // Redirect mail to huntdesign.co.nz
+    $conf['mail_redirect_domain'] = 'huntdesign.co.nz';
+
+    // Environment & Environment Indicator.
+    $conf['environment_indicator_overwrite'] = TRUE;
+    $conf['environment_indicator_overwritten_name'] = strtoupper($conf['environment']);
+    $conf['environment_indicator_overwritten_color'] = '#fa630a';
+    $conf['environment_indicator_overwritten_drawer_color'] = '#333333';
+    $conf['environment_indicator_overwritten_text_color'] = '#ffffff';
+    $conf['environment_indicator_overwritten_position'] = 'top';
+    $conf['environment_indicator_overwritten_fixed'] = FALSE;
+    break;
+
+  // Simplehost 'staging'
+  case 'uat':
     ini_set('error_reporting', E_ALL);
     ini_set('display_errors', TRUE);
     ini_set('memory_limit', '156M');
